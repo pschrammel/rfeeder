3831b02cb0bb99ab2e65c1b673db9ea485a1f8e9
Peter Schrammel <peter.schrammel@gmx.de>
2013-12-08 22:05:42 +0100

    initial bare rails project tree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rfeeder::Application.config.secret_key_base = '26862b5d93880cdc5ee17f901a239019c5c89aae97e8282ee7d548cfff0613b912528d5fa2b7856ddb793992f65c64beaec282adf62deac22dd1932464af985f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rfeeder::Application.config.session_store :cookie_store, key: '_rfeeder_session'
